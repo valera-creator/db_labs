b12b7b02a1fdeb62dbfd88614add7cb5265e0267
Valera <valerylarionov06@gmail.com>
2026-09-13 16:42:43 +0300

    2 лаба: 1 задание

--- laba2/create_table.sql
+++ laba2/create_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE solution
 (
-	card INT PRIMARY KEY NOT NULL,
-	name VARCHAR(30) NOT NULL,
-	surname VARCHAR(30) NOT NULL,
-	answer TEXT,
-	score DECIMAL(3,2) NOT NULL, -- DECIMAL (3,2): 3 цифры в числе всего, две после запятой
-	review VARCHAR(250),
-	has_pass ENUM ('T', 'F')
+    card     INT PRIMARY KEY NOT NULL,
+    name     VARCHAR(30)     NOT NULL,
+    surname  VARCHAR(30)     NOT NULL,
+    answer   TEXT,
+    score    DECIMAL(3, 2)   NOT NULL CHECK (score >= 0 AND score <= 5), -- DECIMAL (3,2): 3 цифры в числе всего, две после запятой
+    review   VARCHAR(250),
+    has_pass ENUM ('T', 'F')
 )
